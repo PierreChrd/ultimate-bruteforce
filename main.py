8b5e8682f356bee567ab168e7286ff8fea0c6ede
Pierre Ch <47186810+PierreChaussard@users.noreply.github.com>
2022-02-24 08:52:02 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#
+# BruteForce Script Version 1.0.0 (2022)
+#
+# This tool may be used for legal purposes only.  Users take full responsibility
+# for any actions performed using this tool. The author accepts no liability for
+# damage caused by this tool.  If these terms are not acceptable to you, then do
+# not use this tool.
+#
+# by Pierre CHAUSSARD & Nathan TEBOUL
+#
+# 
+
